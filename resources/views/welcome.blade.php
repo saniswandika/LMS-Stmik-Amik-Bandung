89bd3397f2550d9e0d5b10b42f4fe00bb22fd790
saniswandika <saniswandikaa@gmail.com>
2022-06-02 06:20:29 +0700

    ganti amik

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/logo.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('/sass/app.css') }}">
-    <title>stmik-amikbandung</title>
+    <title>stmik-bandung</title>
 </head>
 
 <body>
